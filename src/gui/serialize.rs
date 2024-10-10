0b4b37afbfe8f31222f887efa0a7f508f4fd4202
jesse <jesse.kruse@outlook.de>
2024-10-10 21:38:12 +0200

    fixed empty init

--- src/gui/serialize.rs
+++ src/gui/serialize.rs
@@ -1,16 +1,16 @@
+use crate::config::Config;
+use crate::gui::gui_logic::OneDaysWork;
+use crate::utils::{compute_hours_and_minutes, compute_should_hours, format_duration};
+use jiff::{SpanRound, Unit, Zoned};
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::env;
 use std::fs;
 use std::path::PathBuf;
-use jiff::{SpanRound, Unit, Zoned};
-use serde::{Deserialize, Serialize};
-use crate::config::{Config};
-use crate::gui::gui_logic::OneDaysWork;
-use crate::utils::{compute_hours_and_minutes, compute_should_hours, format_duration};
 
 #[derive(Serialize, Deserialize)]
 pub struct Calendar {
-    pub work_days: Vec<OneDaysWork>
+    pub work_days: Vec<OneDaysWork>,
 }
 
 impl Calendar {
@@ -44,26 +44,34 @@ pub fn init_calendar() -> HashMap<String, OneDaysWork> {
     match read_calendar(&path) {
         None => {
             let mut calendar = HashMap::new();
-            calendar.insert(today.to_string(), OneDaysWork {
-                date: today,
-                work_duration: vec![],
-                sum_work: None,
-                sum_pause: None,
-            });
+            calendar.insert(
+                today.to_string(),
+                OneDaysWork {
+                    date: today,
+                    work_duration: vec![],
+                    sum_work: None,
+                    sum_pause: None,
+                },
+            );
             calendar
-        },
-        Some(mut calendar) => {
-            if let Some(last_work_day) = calendar.work_days.last() {
-                if last_work_day.date == today {
-                    return calendar.to_hashmap()                    }
+        }
+        Some(calendar) => {
+            let mut calendar = calendar.to_hashmap();
+            match calendar.get_mut(&Zoned::now().date().to_string()) {
+                Some(_) => calendar,
+                None => {
+                    calendar.insert(
+                        today.to_string(),
+                        OneDaysWork {
+                            date: today,
+                            work_duration: vec![],
+                            sum_work: None,
+                            sum_pause: None,
+                        },
+                    );
+                    return calendar;
+                }
             }
-            calendar.work_days.push(OneDaysWork {
-                date: today,
-                work_duration: vec![],
-                sum_work: None,
-                sum_pause: None,
-            });
-            calendar.to_hashmap()
         }
     }
 }
@@ -74,14 +82,13 @@ pub fn read_calendar(path: &PathBuf) -> Option<Calendar> {
             print!("{}", error);
             None
         }
-        Ok(file_content) => {
-            Some(serde_json::from_str(&file_content).expect("Could not deserialize .work_data.json"))
-        }
-    }   
+        Ok(file_content) => Some(
+            serde_json::from_str(&file_content).expect("Could not deserialize .work_data.json"),
+        ),
+    }
 }
 
 pub fn export(config: &Config) {
-
     let path_buf = rfd::FileDialog::new()
         .set_file_name("work_times_export.csv")
         // TODO: anderes default dir setzen
@@ -99,7 +106,10 @@ pub fn export(config: &Config) {
             let mut total_worked: f32 = 0.;
             for work_day in work_days.work_days.into_iter() {
                 if let Some(sum_work) = work_day.sum_work {
-                    let minutes = sum_work.round(SpanRound::new().largest(Unit::Minute)).unwrap().get_minutes() as f32;
+                    let minutes = sum_work
+                        .round(SpanRound::new().largest(Unit::Minute))
+                        .unwrap()
+                        .get_minutes() as f32;
                     total_worked += minutes / 60.;
                 }
                 write_string = write_string + &serialize_to_csv(work_day, config, total_worked);
@@ -107,48 +117,49 @@ pub fn export(config: &Config) {
         }
         fs::write(path_buf, write_string).unwrap();
     }
-
 }
 
-
-fn serialize_to_csv(todays_work: OneDaysWork, config: &Config, sum_til_last_day: f32) -> String{
-
+fn serialize_to_csv(todays_work: OneDaysWork, config: &Config, sum_til_last_day: f32) -> String {
     let should_hours = compute_should_hours(config);
 
     let date = todays_work.date;
-    
+
     let sum_work = match todays_work.sum_work.as_ref() {
         Some(sum_work) => &format_duration(&sum_work),
-        None => ""
+        None => "",
     };
     let sum_pause = match todays_work.sum_pause.as_ref() {
         Some(sum_pause) => &format_duration(&sum_pause),
-        None => ""
+        None => "",
     };
     let contingent = sum_til_last_day - should_hours;
     let (hours, minutes) = compute_hours_and_minutes(contingent);
 
     let mut write_string = format!("{date};;;;;;\n");
-    write_string = write_string + &format!(";SUM WORK;{sum_work};SUM BREAKS;{sum_pause};CONTINGENT;{hours} : {:0>2}\n", minutes.abs());
+    write_string = write_string
+        + &format!(
+            ";SUM WORK;{sum_work};SUM BREAKS;{sum_pause};CONTINGENT;{hours} : {:0>2}\n",
+            minutes.abs()
+        );
     write_string = write_string + ";;;;;;\n";
     write_string = write_string + ";START;END;;DURATION;BREAK;\n";
-    
+
     for work_times in &todays_work.work_duration {
         let start = match work_times.start.as_ref() {
             Some(start) => &start.time().round(Unit::Minute).unwrap().to_string()[..5],
-            None => ""
+            None => "",
         };
         let end = match work_times.end.as_ref() {
             Some(end) => &end.time().round(Unit::Minute).unwrap().to_string()[..5],
-            None => ""
+            None => "",
         };
         let duration = match work_times.duration.as_ref() {
             Some(duration) => &format_duration(&duration),
-            None => ""
+            None => "",
         };
         let pause = match work_times.pause.as_ref() {
             Some(pause) => &format_duration(&pause),
-            None => ""
+            None => "",
         };
         write_string = write_string + &format!(";{start};{end};;{duration};{pause};\n")
     }
@@ -156,3 +167,4 @@ fn serialize_to_csv(todays_work: OneDaysWork, config: &Config, sum_til_last_day:
     write_string = write_string + "\n";
     write_string
 }
+
