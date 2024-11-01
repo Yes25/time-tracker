30402bc386309748fc48e9fd23d648a23d8dab3e
jesse <jesse.kruse@posteo.de>
2024-11-01 22:59:39 +0100

    new version of workday computation

--- src/tests.rs
+++ src/tests.rs
@@ -1,11 +1,13 @@
 #[cfg(test)]
 mod tests {
     use jiff::Span;
-    use crate::config::get_config;
+    use jiff::civil::Date;
+    use crate::config::Config;
+    use crate::utils::{get_num_workdays};
 
     #[test]
     fn test_spans() {
-        let config = get_config();
+        let config = Config::get_config();
 
         let hours = (config.hours_week / 5.).trunc() as i64;
         println!("{}", hours);
@@ -20,5 +22,24 @@ mod tests {
         dbg!(added);
         assert_eq!(added.get_hours(), 10);
         assert_eq!(added.get_minutes(), 5);
+    }
+
+    #[test]
+    fn test_get_work_days() {
+        let today = Date::new(2024, 11, 01).unwrap();
+
+        let start_day =  Date::new(2024, 10, 11).unwrap();
+        assert_eq!(get_num_workdays(start_day, today), 16.);
+
+        let start_day_2 =  Date::new(2024, 10, 24).unwrap();
+        assert_eq!(get_num_workdays(start_day_2, today), 7.);
+
+        let from =  Date::new(2024, 11, 1).unwrap();
+        let to =  Date::new(2024, 11, 10).unwrap();
+        assert_eq!(get_num_workdays(from, to), 6.);
+
+        let from =  Date::new(2024, 11, 4).unwrap();
+        let to =  Date::new(2024, 11, 6).unwrap();
+        assert_eq!(get_num_workdays(from, to), 3.);
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,6 @@
 use iced_aw::date_picker::Date;
 use jiff::{Span, Zoned};
-use jiff::civil::date;
+use jiff::civil::{date, Weekday};
 use crate::config::Config;
 
 pub fn format_duration(span: &Span) -> String {
@@ -34,4 +34,46 @@ pub fn compute_should_hours(config: &Config) -> f32 {
 
 pub fn jiff_date_from_picker(picker_date: Date) -> jiff::civil::Date {
     date(picker_date.year as i16, picker_date.month as i8, picker_date.day as i8)
+}
+
+
+pub fn get_num_workdays(from: jiff::civil::Date, to: jiff::civil::Date) -> f32 {
+    let work_span = from.until(to).unwrap();
+    let work_days = work_span.get_days() as f32;
+    
+    if work_days < 5. {
+        match from.weekday() {
+            Weekday::Saturday => work_days,
+            Weekday::Sunday => work_days - 1.,
+            _ => work_days + 1.
+        }
+    } else {
+        let first_week_workdays = num_workdays_first_week(from.weekday());
+        let this_week_workdays = num_workdays_this_week_workdays(to.weekday());
+        let full_weeks = ((work_days / 7.).trunc() - 1.).max(0.);         // -2 because without first week and this week
+
+        first_week_workdays + full_weeks * 5. + this_week_workdays
+    }
+}
+
+fn num_workdays_first_week(workday: Weekday) -> f32 {
+    match workday {
+        Weekday::Monday => 5.,
+        Weekday::Tuesday => 4.,
+        Weekday::Wednesday => 3.,
+        Weekday::Thursday => 2.,
+        Weekday::Friday => 1.,
+        _ => 0.
+    }
+}
+
+fn num_workdays_this_week_workdays(workday: Weekday) -> f32 {
+    match workday {
+        Weekday::Monday => 1.,
+        Weekday::Tuesday => 2.,
+        Weekday::Wednesday => 3.,
+        Weekday::Thursday => 4.,
+        Weekday::Friday => 5.,
+        _ => 5.
+    }
 }
